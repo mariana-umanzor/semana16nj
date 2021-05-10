228066a553c9937125d4397035dd5de82c6cca10
Mariana <mariana.umanzor.ugb@gmail.com>
2021-05-10 12:58:46 -0600

    exportar y recibir datos

---
+++ exportar.js
@@ -0,0 +1,7 @@
+let nombre = ["Mariana", "Luisa", "Fernanda"];
+let edad = ["22", "20", "32"];
+
+module.exports = {
+    arreglo_nombre: nombre,
+    erreglo_edad: edad
+}
---
+++ recibir_arreglos.js
@@ -0,0 +1,9 @@
+
+
+//De este modo no me sale el ejercicio
+
+let op = require("./exportar.js")
+
+let nombres = op.arreglo_nombre;
+let edades = op.erreglo_edad
+
